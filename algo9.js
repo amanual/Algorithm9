d3e21063f894c8c7a1f2cafa5181176ed7135882
Amanual <amanual2@gmail.com>
2017-11-27 08:43:29 -0500

    algo9

---
+++ algo9.js
@@ -0,0 +1,7 @@
+//Square the Values (change the value of each array value to its' square)
+var array3 = [1, 2, 3, 7, 3, 4, 5, 8, 9, 10, 30];
+for (var i = 0; i < array3.length; i++) {
+    array3[i] = array3[i] * array3[i];
+
+}
+console.log(array3);
